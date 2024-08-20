41f580888c686a753aa9e0e3240a6c1324d75244
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-20 15:49:05 +0200

    implemented task 1 Character Card Component

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,44 @@
-export function createCharacterCard() {}
+// const imageSrc = "CHANGE"
+// const altValue = "CHANGE"
+// const nameCharacter = "CHANGE"
+// const statusType = "CHANGE"
+// const typeValue = "CHANGE"
+// const occurrencesValue = "CHANGE"
+
+export function createCharacterCard(
+  imageSrc,
+  altValue,
+  nameCharacter,
+  statusType,
+  typeValue,
+  occurrencesValue
+) {
+  const dynamicCards = `
+    <ul class="card-container" data-js="card-container">
+        <li class="card">
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src="${imageSrc}"
+              alt="${altValue}"
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${nameCharacter}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${statusType}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${typeValue}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurrencesValue}</dd>
+            </dl>
+          </div>
+        </li>
+      </ul>
+    `;
+  return dynamicCards;
+}
+
+// document.body.innerHTML += dynamicCards - nicht vergessen zu appenden falls das nicht woanders passiert
