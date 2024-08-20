3ef95d0daebbe535392ce771652ab4f2be3e4d91
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-20 17:49:18 +0200

    solved task 2 and started with 3

--- components/card/card.js
+++ components/card/card.js
@@ -5,34 +5,42 @@
 // const typeValue = "CHANGE"
 // const occurrencesValue = "CHANGE"
 
-export function createCharacterCard(
-  imageSrc,
-  altValue,
-  nameCharacter,
-  statusType,
-  typeValue,
-  occurrencesValue
-) {
-  const dynamicCards = `
+export function createCharacterCard(image, name, status, type, occurrences) {
+  console.log(
+    "Parameters Create Character Card, ",
+    "\nimage",
+    image,
+    "\nname",
+    name,
+    "\nstatus",
+    status,
+    "\ntype",
+    type,
+    "\noccurrences",
+    occurrences
+  );
+  const dynamicCards = document.createElement("li");
+  dynamicCards.classList.add("card-container");
+  dynamicCards.innerHTML = `
     <ul class="card-container" data-js="card-container">
         <li class="card">
           <div class="card__image-container">
             <img
               class="card__image"
-              src="${imageSrc}"
-              alt="${altValue}"
+              src="${image}"
+              alt="${name}"
             />
             <div class="card__image-gradient"></div>
           </div>
           <div class="card__content">
-            <h2 class="card__title">${nameCharacter}</h2>
+            <h2 class="card__title">${name}</h2>
             <dl class="card__info">
               <dt class="card__info-title">Status</dt>
-              <dd class="card__info-description">${statusType}</dd>
+              <dd class="card__info-description">${status}</dd>
               <dt class="card__info-title">Type</dt>
-              <dd class="card__info-description">${typeValue}</dd>
+              <dd class="card__info-description">${type}</dd>
               <dt class="card__info-title">Occurrences</dt>
-              <dd class="card__info-description">${occurrencesValue}</dd>
+              <dd class="card__info-description">${occurrences}</dd>
             </dl>
           </div>
         </li>
--- index.js
+++ index.js
@@ -9,6 +9,39 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
+const maxPage = 42;
 const page = 1;
 const searchQuery = "";
+
+import { createCharacterCard } from "./components/card/card.js";
+
+async function fetchCharacters() {
+  try {
+    cardContainer.innerHTML = "";
+    const url = `https://rickandmortyapi.com/api/character/?page=${page}`;
+    const response = await fetch(url);
+    console.log("response", response);
+
+    const responseJSON = await response.json(); //converting our response to JSON to gain access to the data we want
+    console.log("responseJSON", responseJSON); // console logging the actual data
+
+    const dataItems = responseJSON.results;
+    console.log("dataItems", dataItems);
+
+    dataItems.forEach((dataItem) => {
+      console.log("dataItem", dataItem);
+      const cards = createCharacterCard(
+        dataItem.image,
+        dataItem.name,
+        dataItem.status,
+        dataItem.type,
+        dataItem.episode.length
+      );
+      cardContainer.append(cards);
+    });
+  } catch (error) {
+    console.log("Sheeesh! Something wrong happened! - ", error);
+  }
+}
+
+fetchCharacters();
