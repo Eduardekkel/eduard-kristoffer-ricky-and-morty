421c2d74598d237437d1938b753b17144cb508bf
Kristoffer Jans <jans.kiki@gmail.com>
2024-08-21 17:16:09 +0200

    added createSearchBar functionality

---
+++ components/search-bar/search-bar.js
@@ -0,0 +1,31 @@
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
+
+export function createSearchBar() {
+  const searchBar = document.createElement("form");
+  searchBar.classList.add("search-bar");
+  searchBar.dataset.js = "search-bar";
+  searchBarContainer.appendChild(searchBar);
+
+  const inputField = document.createElement("input");
+  inputField.classList.add("search-bar__input");
+  inputField.name = "query";
+  inputField.type = "text";
+  inputField.placeholder = "search characters";
+  inputField.setAttribute("aria-label", "character name");
+  searchBar.appendChild(inputField);
+
+  const searchButton = document.createElement("button");
+  searchButton.classList.add("search-bar__button");
+  searchButton.setAttribute("aria-label", "search for character");
+  searchBar.appendChild(searchButton);
+
+  const searchIcon = document.createElement("img");
+  searchIcon.classList.add("search-bar__icon");
+  searchIcon.src = "./assets/magnifying-glass.png";
+  searchIcon.alt = "";
+  searchButton.appendChild(searchIcon);
+
+  return searchBar;
+}
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import { createCharacterCard } from "./components/card/card.js";
 import { createNextButton } from "./components/nav-button/nav-button.js";
 import { createPrevButton } from "./components/nav-button/nav-button.js";
 import { createPagination } from "./components/nav-pagination/nav-pagination.js";
+import { createSearchBar } from "./components/search-bar/search-bar.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+//const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 //const prevButton = document.querySelector('[data-js="button-prev"]');
 //const nextButton = document.querySelector('[data-js="button-next"]');
@@ -17,6 +18,8 @@ const navigation = document.querySelector('[data-js="navigation"]');
 let maxPage = 42;
 let page = 1;
 let searchQuery = "";
+
+const searchBar = createSearchBar();
 
 // Fetching Characters
 async function fetchCharacters() {
